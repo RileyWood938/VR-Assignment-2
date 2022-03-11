f757c29087e2e6624f9299fc4e1349d81016e874
VladTsimoshchanka <78988772+VladTsimoshchanka@users.noreply.github.com>
2022-03-10 23:09:58 -0500

    Added a script for screen and screen itself

---
+++ VRAssignment2/Assets/Scripts/ScreenBehavior.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBehavior : MonoBehaviour
+{
+
+    float GracePeriod = 4.0f;
+    float TrialGenerationInterval = 0.5f;
+    bool countingGracePeriod = false;
+    bool countingPredicament = false;
+    public Material normal;
+    public Material red;
+    public Material orange;
+    public Material yellow;
+    public Material green;
+    public Material blue;
+    public Material purple;
+    public Material[] colors;
+    public Material colorOfProblem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countingGracePeriod = true;
+        colors = new Material[] { red, orange, yellow, green, blue, purple };
+    }
+    public void StopAndRefresh()
+    {
+        GracePeriod = 4f;
+        countingGracePeriod = false;
+
+    }
+    public void startCounting()
+    {
+        countingGracePeriod = true;
+    }
+
+    public void GeneratePredicament()
+    {
+        colorOfProblem = colors[Random.Range(0, 6)];
+        GetComponent<Renderer>().material = colorOfProblem;
+    }
+
+    public void SolvePredicament()
+    {
+        GetComponent<Renderer>().material = normal;
+    }
+    public void StartTrial()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (countingGracePeriod)
+        {
+
+            GracePeriod -= Time.deltaTime;
+            if (GracePeriod <= 0)
+            {
+                StopAndRefresh();
+                countingPredicament = true;
+            }
+
+        }
+        if (countingPredicament)
+        {
+            if(TrialGenerationInterval > 0f)
+            {
+                TrialGenerationInterval -= Time.deltaTime;
+            }
+            else
+            {
+                TrialGenerationInterval = 0.5f;
+                //Debug.Log("I am here");
+                if(Random.Range(0, 2) > 0)
+                {
+                    countingPredicament = false;
+                    GeneratePredicament();
+                }
+            }
+                
+
+        }
+    }
+}
