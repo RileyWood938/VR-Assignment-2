cf96213d18f6d5ff04ded6e59f149bd1e4c4cff1
RileyWood938 <71392554+RileyWood938@users.noreply.github.com>
2022-03-12 14:23:32 -0500

    Added Room, Button
    Added a button witha  script that plays a pressed animation when "e" is pressed, or when the google cardboard button is pressed while looking at it.

---
+++ VRAssignment2/Assets/Scripts/ButtonPressAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPressAnimation : MonoBehaviour
+{
+    new Animator animator;
+    new bool lookedAt = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("e") && lookedAt)
+        {
+            animator.SetBool("Pressed", true);
+        }
+        else
+        {
+            animator.SetBool("Pressed", false);
+        }
+    }
+    public void pressed()
+    {
+        animator.SetBool("Pressed", true);
+    }
+    public void GazeOver()
+    {
+        lookedAt = true;
+    }
+    public void GazeEnd()
+    {
+        lookedAt = false;
+    }
+}
