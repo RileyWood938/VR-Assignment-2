582e96b8c9a563a2aa719feaab5dbe20ca69b671
VladTsimoshchanka <78988772+VladTsimoshchanka@users.noreply.github.com>
2022-03-14 16:51:53 -0400

    Added level2 difficulty

--- VRAssignment2/Assets/Scripts/GameControl.cs
+++ VRAssignment2/Assets/Scripts/GameControl.cs
@@ -12,15 +12,30 @@ public class GameControl : MonoBehaviour
     public bool buttonSelection = false;
     public GameObject TentativeButton;
     public Material comparedColor;
+    public bool level2;
     // Start is called before the first frame update
     void Start()
     {
         scoreTracker.text = "Score: " + score.ToString();
         //screens = new GameObject[];
         screens = GameObject.FindGameObjectsWithTag("Screen");
+        RandomizeScreens();
         buttons = GameObject.FindGameObjectsWithTag("Button");
     }
-
+    public void RandomizeScreens()
+    {
+        for (int i = 0; i < screens.Length; i++)
+        {
+            GameObject screen = screens[i];
+            int r = Random.Range(0, i);
+            screens[i] = screens[r];
+            screens[r] = screen;
+        }
+        for(int i = 0; i < 3; i++)
+        {
+            screens[i].GetComponent<ScreenBehavior>().Slumber();
+        }
+    }
     public void tieButtons(GameObject screen)
     {
         for(int i = 0; i < buttons.Length; i++)
@@ -35,6 +50,7 @@ public class GameControl : MonoBehaviour
             TentativeButton = buttons[i];
             if (TentativeButton.GetComponent<ButtonScript>().isChanged() == false)
             {
+                //Debug.Log("ButtonTied");
                 TentativeButton.GetComponent<ButtonScript>().changeColor(screen.GetComponent<ScreenBehavior>().fetchColor());
                 break;
             }
@@ -43,13 +59,24 @@ public class GameControl : MonoBehaviour
 
     public void checkButton(GameObject button)
     {
+        
         for (int i = 0; i < screens.Length; i++)
         {
+            
             if(screens[i].GetComponent<ScreenBehavior>().fetchColor() == button.GetComponent<ButtonScript>().fetchColor())
             {
+                //Debug.Log("ScreenCleared");
                 screens[i].GetComponent<ScreenBehavior>().SolvePredicament();
                 button.GetComponent<ButtonScript>().switchColorToNormal();
                 score += 10;
+                if (score >= 100 && !level2)
+                {
+                    level2 = true;
+                    foreach (GameObject screen in screens)
+                    {
+                        screen.GetComponent<ScreenBehavior>().Awake();
+                    }
+                }
                 scoreTracker.text = "Score: " + score.ToString();
                 break;
             }
